5d343f33de1dce4b2df271000e31868b9bac9962
Eric <eric.wu@duke.edu>
2014-04-21 01:38:55 -0400

    fix dfs

--- tests/DeTest.java
+++ tests/DeTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.util.Arrays;
+
 import common.*;
 import dblockcache.*;
 import dfs.*;
@@ -10,15 +12,17 @@ public class DeTest {
 		DeDFS myDFS = new DeDFS();
 		myDFS.init();
 		DFileID file1 = myDFS.createDFile();
-		//int file1ID = file1.getDFileID();
-		byte[] myWriteBuffer = new byte[2];
-		byte[] buffer = new byte[2];
-		myWriteBuffer[0] = 'a';
-		myWriteBuffer[1] = 'b';
+		String contents = "";
+		for (int i = 0; i < 10000-1; i++) {
+			contents += (char) i;
+		}
+		byte[] in = contents.getBytes();
+		byte[] out = new byte[in.length];
 		System.out.println("Writing...");
-		myDFS.write(file1, myWriteBuffer, 0, 2);
+		myDFS.write(file1, in, 0, in.length);
 		System.out.println("Reading...");
-		myDFS.read(file1, buffer, 0, 2);
-		System.out.println(buffer[0]+" "+buffer[1]);
+		myDFS.read(file1, out, 0, out.length);
+		String s = new String(out);
+		System.out.println(s.charAt(9000));
 	}
 }
